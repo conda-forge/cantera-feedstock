9d5e39be7425d6a72bb27c7512c2573c7e0fcdd9
Isuru Fernando <isuruf@gmail.com>
2021-02-09 11:51:14 -0600

    make conda-build complain less

--- recipe/build_lib.sh
+++ recipe/build_lib.sh
@@ -4,7 +4,7 @@ echo "DELETING files from devel except shared libraries"
 echo "****************************"
 
 rm -rf $PREFIX/share
-rm -rf $PREFIX/include
+rm -rf $PREFIX/include/cantera
 rm -rf $PREFIX/bin
 rm -rf $PREFIX/lib/pkg-config
 rm -rf $PREFIX/lib/libcantera.a
