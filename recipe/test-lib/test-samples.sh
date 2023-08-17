b50d32c1c76b9724b1c8f15e41340edbb0875b93
Ray Speth <speth@mit.edu>
2023-08-16 21:01:31 -0400

    Show generated build scripts

--- recipe/test-lib/test-samples.sh
+++ recipe/test-lib/test-samples.sh
@@ -14,6 +14,9 @@ cp -a ${CONDA_PREFIX}/share/cantera/samples/cxx/demo cxx_demo
 
 echo -e '\n***** Testing clib example with CMake *****\n'
 cd ${CONDA_PREFIX}/share/cantera/samples/clib
+echo "==="
+cat CMakeLists.txt
+echo "==="
 mkdir build
 cd build
 cmake ..
@@ -22,11 +25,17 @@ cmake --build .
 
 echo -e '\n***** Testing clib example with SCons *****\n'
 cd ${CONDA_PREFIX}/share/cantera/samples/clib
+echo "==="
+cat SConstruct
+echo "==="
 scons
 ./demo || die "clib-scons failed"
 
 echo -e '\n***** Testing kinetics1 example with CMake *****\n'
 cd ${CONDA_PREFIX}/share/cantera/samples/cxx/kinetics1
+echo "==="
+cat CMakeLists.txt
+echo "==="
 mkdir build
 cd build
 cmake ..
@@ -35,16 +44,25 @@ cmake --build .
 
 echo -e '\n***** Testing kinetics1 example with SCons *****\n'
 cd ${CONDA_PREFIX}/share/cantera/samples/cxx/kinetics1
+echo "==="
+cat SConstruct
+echo "==="
 scons
 ./kinetics1 || die "kinetics1-scons failed"
 
 echo -e '\n***** Testing rankine example with pkg-config *****\n'
 cd ${CONDA_PREFIX}/share/cantera/samples/cxx/rankine
+echo "==="
+echo $(pkg-config --cflags cantera --libs)
+echo "==="
 ${CXX} rankine.cpp -o rankine $(pkg-config --cflags cantera --libs)
 ./rankine || die "rankine-pkgconfig failed"
 
 echo -e '\n***** Testing openmp_ignition example with CMake *****\n'
 cd ${CONDA_PREFIX}/share/cantera/samples/cxx/openmp_ignition
+echo "==="
+cat CMakeLists.txt
+echo "==="
 mkdir build
 cd build
 cmake ..
@@ -53,6 +71,9 @@ cmake --build .
 
 echo -e '\n***** Testing openmp_ignition example with SCons *****\n'
 cd ${CONDA_PREFIX}/share/cantera/samples/cxx/openmp_ignition
+echo "==="
+cat SConstruct
+echo "==="
 scons
 ./openmp_ignition || die "openmp_ignition-scons failed"
 
@@ -63,6 +84,9 @@ cp ${BASEDIR}/test-lib/extensible-rate.cpp cxx_demo/demo.cpp
 
 echo -e '\n***** Testing ExtensibleRate with CMake *****\n'
 cd ${BASEDIR}/cxx_demo
+echo "==="
+cat CMakeLists.txt
+echo "==="
 mkdir build
 cd build
 cmake ..
@@ -73,6 +97,9 @@ cp ${BASEDIR}/test-lib/extensible-reactions.yaml .
 
 echo -e '\n***** Testing ExtensibleRate with SCons *****\n'
 cd ${BASEDIR}/cxx_demo
+echo "==="
+cat SConstruct
+echo "==="
 scons
 cp ${BASEDIR}/test-lib/user_ext.py .
 cp ${BASEDIR}/test-lib/extensible-reactions.yaml .
