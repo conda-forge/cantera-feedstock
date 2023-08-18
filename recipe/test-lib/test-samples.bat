0eb34be38eb1a2f0a32731ad7a0a6600464fb593
Ray Speth <speth@mit.edu>
2023-08-17 22:29:21 -0400

    Fix .bat file issues

--- recipe/test-lib/test-samples.bat
+++ recipe/test-lib/test-samples.bat
@@ -1,11 +1,11 @@
 echo on
 
 rem Test clib example with CMake
-echo -e "\n***** Testing clib example with CMake *****\n"
-cd "%CONDA_PREFIX%\share\cantera\samples\clib"
-echo "==="
+echo ***** Testing clib example with CMake *****
+cd /d "%CONDA_PREFIX%\share\cantera\samples\clib"
+echo ===
 type CMakeLists.txt
-echo "==="
+echo ===
 mkdir build
 cd build
 cmake ..
@@ -15,22 +15,22 @@ if errorlevel 1 exit 1
 echo "SUCCESS!"
 
 rem Test clib example with SCons
-echo -e "\n***** Testing clib example with SCons *****\n"
-cd "%CONDA_PREFIX%\share\cantera\samples\clib"
-echo "==="
+echo ***** Testing clib example with SCons *****
+cd /d "%CONDA_PREFIX%\share\cantera\samples\clib"
+echo ===
 type SConstruct
-echo "==="
+echo ===
 scons
 demo.exe
 if errorlevel 1 exit 1
 echo "SUCCESS!"
 
 rem Test kinetics1 example with CMake
-echo -e "\n***** Testing kinetics1 example with CMake *****\n"
-cd "%CONDA_PREFIX%\share\cantera\samples\cxx\kinetics1"
-echo "==="
+echo ***** Testing kinetics1 example with CMake *****
+cd /d "%CONDA_PREFIX%\share\cantera\samples\cxx\kinetics1"
+echo ===
 type CMakeLists.txt
-echo "==="
+echo ===
 mkdir build
 cd build
 cmake ..
@@ -40,22 +40,22 @@ if errorlevel 1 exit 1
 echo "SUCCESS!"
 
 rem Test kinetics1 example with SCons
-echo -e "\n***** Testing kinetics1 example with SCons *****\n"
-cd "%CONDA_PREFIX%\share\cantera\samples\cxx\kinetics1"
-echo "==="
+echo ***** Testing kinetics1 example with SCons *****
+cd /d "%CONDA_PREFIX%\share\cantera\samples\cxx\kinetics1"
+echo ===
 type SConstruct
-echo "==="
+echo ===
 scons
 kinetics1.exe
 if errorlevel 1 exit 1
 echo "SUCCESS!"
 
 rem Test openmp_ignition example with CMake
-echo -e "\n***** Testing openmp_ignition example with CMake *****\n"
-cd "%CONDA_PREFIX%\share\cantera\samples\cxx\openmp_ignition"
-echo "==="
+echo ***** Testing openmp_ignition example with CMake *****
+cd /d "%CONDA_PREFIX%\share\cantera\samples\cxx\openmp_ignition"
+echo ===
 type CMakeLists.txt
-echo "==="
+echo ===
 mkdir build
 cd build
 cmake ..
@@ -65,11 +65,11 @@ if errorlevel 1 exit 1
 echo "SUCCESS!"
 
 rem Test openmp_ignition example with SCons
-echo -e "\n***** Testing openmp_ignition example with SCons *****\n"
-cd "%CONDA_PREFIX%\share\cantera\samples\cxx\openmp_ignition"
-echo "==="
+echo ***** Testing openmp_ignition example with SCons *****
+cd /d "%CONDA_PREFIX%\share\cantera\samples\cxx\openmp_ignition"
+echo ===
 type SConstruct
-echo "==="
+echo ===
 scons
 openmp_ignition.exe
 if errorlevel 1 exit 1
