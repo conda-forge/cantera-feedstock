c060083812dae45cc3e40a4d5960ba593ff0d3f1
Ray Speth <speth@mit.edu>
2023-08-17 20:17:38 -0400

    Fix path issues with ExtensibleRate test

--- recipe/test-lib/test-extensiblerate.bat
+++ recipe/test-lib/test-extensiblerate.bat
@@ -2,6 +2,8 @@ echo on
 
 rem Get the current working directory
 set "BASEDIR=%cd%"
+set "PYTHONPATH=%PYTHONPATH%;%BASEDIR%\test-lib"
+set "CANTERA_DATA=%BASEDIR%\test-lib"
 
 copy /y "%CONDA_PREFIX%\share\cantera\samples\cxx\demo" "cxx_demo"
 copy "%BASEDIR%\test-lib\extensible-rate.cpp" "cxx_demo\demo.cpp"
@@ -16,8 +18,6 @@ mkdir build
 cd build
 cmake ..
 cmake --build . --config Release
-copy "%BASEDIR%\test-lib\user_ext.py" .
-copy "%BASEDIR%\test-lib\extensible-reactions.yaml" .
 demo.exe
 if errorlevel 1 exit 1
 echo "SUCCESS!"
@@ -29,5 +29,6 @@ echo "==="
 type SConstruct
 echo "==="
 scons
+demo.exe
 if errorlevel 1 exit 1
 echo "SUCCESS!"
