b9461d00a43341276c794d9388d7f709ae5ad780
Ray Speth <speth@mit.edu>
2023-08-20 10:40:51 -0400

    Minifying example

--- recipe/build.sh
+++
@@ -1,9 +0,0 @@
-set +x
-
-echo "****************************"
-echo "LIBRARY BUILD STARTED"
-echo "****************************"
-
-echo "****************************"
-echo "BUILD COMPLETED SUCCESSFULLY"
-echo "****************************"
