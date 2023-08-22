2354fc3feb6a77cfeb2b10d15427c04c23474393
Ray Speth <speth@mit.edu>
2023-08-22 10:00:54 -0400

    Add more info for bug report

--- recipe/build_lib.sh
+++ recipe/build_lib.sh
@@ -1,1 +1,9 @@
 echo "Hello from build_lib.sh"
+
+echo "~~~~~~~~~~ 'conda list' output ~~~~~~~~~~"
+conda list
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+echo "~~~~~~~~~~ 'conda info' output ~~~~~~~~~~"
+conda info
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
